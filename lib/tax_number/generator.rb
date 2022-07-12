a8ca67db35fd033e717bffee27f8c6835650f234
RobertChang <robertchang0722@gmail.com>
2022-07-12 14:50:13 +0800

    feature: completed taiwan tax number generator

---
+++ lib/tax_number/generator.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module TaxNumber
+  module Generator
+    MULTIPLIER = [1,2,1,2,1,2,4,1].freeze
+
+    def self.tax_number
+      validate = false
+
+      while !validate do
+        tax_number = 8.times.map { rand(1..9) }.join
+        validate = true if valid?(tax_number)
+      end
+
+      tax_number
+    end
+
+    private
+
+    def self.valid?(string)
+      ubn = string.to_s 
+
+      return false if !( ubn =~ /^\d+$/ )
+
+      digits = ubn.chars.map(&:to_i)
+
+      special_sum = calculate_special_sum(digits)
+
+      if special_sum % 10 == 0
+        return true
+      else
+        if digits[6] == 7 && special_sum % 10 == 9
+          return true
+        else
+          return false
+        end
+      end
+    end
+
+    def self.calculate_special_sum(digits)
+      sum = 0
+      8.times do |i|
+        m = digits[i] * MULTIPLIER[i]
+        sum += m.divmod(10).inject(0, &:+)
+      end
+      sum
+    end
+  end
+end
---
+++ tax_number_tw.gemspec
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Gem::Specification.new do |s|
+  s.name        = "tax_number_tw"
+  s.version     = "0.0.1"
+  s.summary     = "Hola!"
+  s.description = "A simple hello world gem"
+  s.authors     = ["RobertChang"]
+  s.email       = "fishppm0212@gmail.com"
+  s.files       = ["lib/tax_number/generator.rb"]
+  s.homepage    = 'https://github.com/5xTraining/tax_number_tw'
+  s.license     = "MIT"
+end
